3f4f02f4781cdea29a26051ee27082df56901a2c
이광훈 <leegh963@naver.com>
2024-04-30 14:11:00 +0900

    Feat : voteControlle 테스트

--- src/main/java/capstone/restaurant/vote/VoteController.java
+++ src/main/java/capstone/restaurant/vote/VoteController.java
@@ -1,20 +1,18 @@
 package capstone.restaurant.vote;
 
-import capstone.restaurant.exceptionHandler.CustomException;
-import capstone.restaurant.exceptionHandler.ErrorResponseDto;
 import capstone.restaurant.vote.dto.GetMemberReturnDto;
+import capstone.restaurant.vote.dto.ResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
 
 @Tag(name = "vote" , description = "투표 관련 api")
 @RestController
@@ -24,10 +22,13 @@ public class VoteController {
     public VoteController(){}
 
     @Operation(summary = "Swagger 예시" , description = "Swagger 예시")
-    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "vote 조회 성공" , content = @Content(schema = @Schema(implementation = GetMemberReturnDto.class)))})
+    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "vote 조회 성공") , @ApiResponse(responseCode = "404" , description = "없음")})
     @GetMapping(value = "/example")
-    public GetMemberReturnDto getMember(@RequestParam(required = false) @Parameter String username , @RequestParam(required = false) @Parameter Integer age){
-        return new GetMemberReturnDto("123" , 12 );
+    public ResponseEntity<ResponseDto<GetMemberReturnDto>> getMember(@RequestParam(required = false) @Parameter String username , @RequestParam(required = false) @Parameter Integer age){
+
+        ResponseDto<GetMemberReturnDto> responseDto = new ResponseDto<>(201 , "CREATED" , new GetMemberReturnDto("132" , 1));
+
+        return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping(value = "/exampl")
