5e472cbb936662bf8a76460838bd27f6b6ba9042
koomin1227 <koomin1227@naver.com>
2024-04-30 17:54:37 +0900

    Feat : GET /restaurant 의 응답 DTO 구현

---
+++ src/main/java/capstone/restaurant/RestaurantListSub.java
@@ -0,0 +1,15 @@
+package capstone.restaurant;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class RestaurantListSub {
+    private String name;
+    private String thumbnail;
+    private List<String> tags;
+    private String restaurantId;
+}
