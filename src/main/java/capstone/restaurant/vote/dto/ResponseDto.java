efd06421ff13e801f606c2b31e3ecc1683d69a07
이광훈 <leegh963@naver.com>
2024-04-30 14:09:50 +0900

    Feat : 응답 형식 통일

---
+++ src/main/java/capstone/restaurant/vote/dto/ResponseDto.java
@@ -0,0 +1,26 @@
+package capstone.restaurant.vote.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDto<T> {
+
+    private Integer statusCode;
+    private String message;
+    private T data;
+
+    public ResponseDto(Integer statusCode , String message){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = null;
+    }
+
+    public ResponseDto(Integer statusCode , String message , T data){
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+}
