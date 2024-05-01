7d6cfce75788dd16c41d9dcec3393b9cce993e19
이광훈 <leegh963@naver.com>
2024-05-01 12:41:52 +0900

    Feat : 상속을 이용해 JpaRepository 에서 queryDSL 사용할 수 있게 함

--- src/main/java/capstone/restaurant/repository/RestaurantRepositoryImpl.java
+++ src/main/java/capstone/restaurant/repository/RestaurantRepositoryImpl.java
@@ -1,10 +1,17 @@
 package capstone.restaurant.repository;
 
 import capstone.restaurant.entity.Restaurant;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import java.util.List;
 
 public class RestaurantRepositoryImpl implements RestaurantRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    public RestaurantRepositoryImpl(JPAQueryFactory queryFactory){
+        this.queryFactory = queryFactory;
+    }
 
     @Override
     public List<Restaurant> findRestaurantListByTagOrKeyWord() {
--- src/main/java/capstone/restaurant/service/RestaurantService.java
+++ src/main/java/capstone/restaurant/service/RestaurantService.java
@@ -1,13 +1,15 @@
 package capstone.restaurant.service;
 
+import capstone.restaurant.repository.RestaurantRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RestaurantService {
 
-    public RestaurantService(){
+    private final RestaurantRepository restaurantRepository;
+    public RestaurantService(RestaurantRepository restaurantRepository){
+        this.restaurantRepository = restaurantRepository;
     }
     public void restaurantListFind(){
-
     }
 }
