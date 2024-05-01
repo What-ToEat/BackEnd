c5ad680b40a36d440fa45d7ccc52721c470979b5
이광훈 <leegh963@naver.com>
2024-05-01 10:47:52 +0900

    Feat : Restaurant 목록 반환 dto 설정

--- src/main/java/capstone/restaurant/controller/RestaurantController.java
+++ src/main/java/capstone/restaurant/controller/RestaurantController.java
@@ -1,11 +1,17 @@
 package capstone.restaurant.controller;
 
 import capstone.restaurant.dto.ResponseDto;
+import capstone.restaurant.dto.RestaurantReturnDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "restaurant" , description = "레스토랑 관련 API")
 @RestController
@@ -14,7 +20,10 @@ public class RestaurantController {
     public RestaurantController(){}
 
     @Operation(summary = "Swagger 예시" , description = "Swagger 예시")
-    public ResponseEntity<ResponseDto<List<>>> findRestaurantList(){
-
+    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "레스토랑 조회 완료") , @ApiResponse(responseCode = "404" , description = "없음")})
+    @GetMapping()
+    public ResponseEntity<ResponseDto<List<RestaurantReturnDto>>> findRestaurantList(){
+        ResponseDto<List<RestaurantReturnDto>> returnDto = new ResponseDto<>(200 , "123");
+        return ResponseEntity.ok(returnDto);
     }
 }
---
+++ src/main/java/capstone/restaurant/dto/RestaurantReturnDto.java
@@ -0,0 +1,13 @@
+package capstone.restaurant.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestaurantReturnDto {
+
+    private String name;
+    private String restaurantImage;
+    private String restaurantId;
+}
