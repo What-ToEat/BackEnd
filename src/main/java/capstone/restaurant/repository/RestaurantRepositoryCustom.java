4df842e5761eafe9172f2759b06ba24f8ba2b137
이광훈 <leegh963@naver.com>
2024-05-04 13:52:17 +0900

    Feat : JPA , QueryDSL 설정

--- src/main/java/capstone/restaurant/repository/RestaurantRepositoryCustom.java
+++ src/main/java/capstone/restaurant/repository/RestaurantRepositoryCustom.java
@@ -1,10 +1,11 @@
 package capstone.restaurant.repository;
 
+import capstone.restaurant.dto.restaurant.RestaurantListSub;
 import capstone.restaurant.entity.Restaurant;
 
 import java.util.List;
 
 public interface RestaurantRepositoryCustom {
 
-    List<Restaurant> findRestaurantListByTagOrKeyWord();
+    List<RestaurantListSub> findRestaurantListByTagOrKeyWord(String place , String[] tags , String search , int page);
 }
--- src/main/java/capstone/restaurant/repository/RestaurantRepositoryImpl.java
+++ src/main/java/capstone/restaurant/repository/RestaurantRepositoryImpl.java
@@ -1,9 +1,17 @@
 package capstone.restaurant.repository;
 
+import capstone.restaurant.dto.restaurant.RestaurantListSub;
+import capstone.restaurant.dto.tag.TagResponse;
 import capstone.restaurant.entity.Restaurant;
+import capstone.restaurant.entity.RestaurantTag;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static capstone.restaurant.entity.QRestaurantTag.restaurantTag;
 
 public class RestaurantRepositoryImpl implements RestaurantRepositoryCustom{
 
@@ -14,7 +22,36 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryCustom{
     }
 
     @Override
-    public List<Restaurant> findRestaurantListByTagOrKeyWord() {
-        return null;
+    public List<RestaurantListSub> findRestaurantListByTagOrKeyWord(String place , String[] tags , String search , int page) {
+
+        List<Restaurant> query = queryFactory.select(restaurantTag.restaurant)
+                .from(restaurantTag)
+                .where(restaurantTag.tag.tagName.in(tags))
+                .groupBy(restaurantTag.restaurant)
+                .having(restaurantTag.restaurant.count().eq((long) tags.length))
+                .offset(page)
+                .limit(5)
+                .fetch();
+
+        List<RestaurantListSub> result = new ArrayList<RestaurantListSub>();
+
+        for (Restaurant item : query){
+            List<TagResponse> tagResponseList = new ArrayList<>();
+
+            for(RestaurantTag tag: item.getRestaurantTag()){
+                tagResponseList.add(new TagResponse(tag.getTag().getTagName() , "양식"));
+            }
+
+            RestaurantListSub rls = new RestaurantListSub();
+            rls.setName(item.getName());
+            rls.setThumbnail(item.getAddress());
+            rls.setTags(tagResponseList);
+            rls.setRestaurantId(item.getRestaurantHash());
+            result.add(rls);
+            System.out.println("rls.toString() = " + rls.toString());
+        }
+
+
+        return result;
     }
 }
