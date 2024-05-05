e2564c60ea65ca5af9f712827c3bb3d49e1a9d79
이광훈 <leegh963@naver.com>
2024-05-05 11:26:12 +0900

    Feat : keyword 검색 controller

--- src/main/java/capstone/restaurant/controller/RestaurantController.java
+++ src/main/java/capstone/restaurant/controller/RestaurantController.java
@@ -23,9 +23,9 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @Operation(summary = "식당 목록 조회", description = "조건에 맞는 식당의 목록을 조회한다.")
-    @GetMapping
+    @GetMapping("/tag")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDto<RestaurantListResponse> getRestaurantList(@Parameter(example = "홍대", description = "조회하고 싶은 장소") @RequestParam(required = false) String place,
+    public ResponseDto<RestaurantListResponse> getRestaurantListByTag(@Parameter(example = "홍대", description = "조회하고 싶은 장소") @RequestParam(required = false) String place,
                                                                  @Parameter(example = "맛집", description = "조회하고 싶은 태그, 여러 태그를 조회하려면 여러번 쓴다.") @RequestParam(required = false) String[] tags,
                                                                  @Parameter(example = "1", description = "조회하려는 페이지 번호, 기본으로 1") @RequestParam(required = false, defaultValue = "1") int page) {
 
@@ -33,6 +33,11 @@ public class RestaurantController {
 
         return new ResponseDto<>(200, "ok", restaurantListResponse);
 
+    }
+
+    @GetMapping("/keyword")
+    public ResponseDto<RestaurantListResponse> getRestaurantListByKeyword(@Parameter(example = "롯데리아" , description = "검색 키워드") @RequestParam(required = false) String keyword){
+        return new ResponseDto<>(200, "ok" , new ResponseDto<RestaurantResponse>);
     }
 
     @Operation(summary = "식당 상세 조회", description = "식당의 상세정보를 조회한다.")
