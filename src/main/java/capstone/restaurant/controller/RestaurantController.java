2ed85132661bfc11ed9d95433bf24e97c80c41e3
이광훈 <leegh963@naver.com>
2024-05-06 21:39:07 +0900

    Feat : RestaurantController 작성

--- src/main/java/capstone/restaurant/controller/RestaurantController.java
+++ src/main/java/capstone/restaurant/controller/RestaurantController.java
@@ -7,6 +7,8 @@ import capstone.restaurant.dto.tag.TagListResponse;
 import capstone.restaurant.service.RestaurantService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -27,6 +29,7 @@ public class RestaurantController {
     @Operation(summary = "식당 목록 조회", description = "조건에 맞는 식당의 목록을 조회한다.")
     @GetMapping("/tag")
     @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "태그로 레스토랑 리스트 조회 성공") , @ApiResponse(responseCode = "400" , description = "page 는 1 이상의 정수이어야함")})
     public ResponseDto<RestaurantListResponse> getRestaurantListByTag(@Parameter(example = "홍대", description = "조회하고 싶은 장소") @RequestParam(required = false) String place,
                                                                  @Parameter(example = "맛집", description = "조회하고 싶은 태그, 여러 태그를 조회하려면 여러번 쓴다.") @RequestParam(required = false) String[] tags,
                                                                  @Parameter(example = "1", description = "조회하려는 페이지 번호, 기본으로 1") @RequestParam(required = false, defaultValue = "1") int page) {
@@ -41,6 +44,8 @@ public class RestaurantController {
 
     @Operation(summary = "식당 검색 조회", description = "검색어로 식당을 조회한다.")
     @GetMapping("/keyword")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "키워드로 레스토랑 리스트 조회 성공") , @ApiResponse(responseCode = "400" , description = "page 는 1 이상의 정수이어야함" )})
     public ResponseDto<RestaurantListResponse> getRestaurantListByKeyword(@Parameter(example = "롯데리아" , description = "검색 키워드") @RequestParam(required = false) String keyword,
                                                                           @Parameter(example = "1", description = "조회하려는 페이지 번호, 기본으로 1") @RequestParam(required = false, defaultValue = "1") int page){
 
@@ -53,7 +58,10 @@ public class RestaurantController {
 
     @Operation(summary = "식당 상세 조회", description = "식당의 상세정보를 조회한다.")
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses(value = { @ApiResponse(responseCode = "200" , description = "태그로 레스토랑 리스트 조회 성공") , @ApiResponse(responseCode = "404" , description = "식당을 찾을 수 없음")})
     public ResponseDto<RestaurantResponse> getRestaurant(@PathVariable String id) {
-        return new ResponseDto<>(200, "ok", new RestaurantResponse());
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+        return new ResponseDto<>(200, "ok", restaurantResponse);
     }
 }
