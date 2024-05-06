53ae747e3c0ba6b1b0cc837effce7f9829c90f83
koomin1227 <koomin1227@naver.com>
2024-05-06 22:34:34 +0900

    Feat: 투표 옵션을 등록하는 기능 구현

--- src/main/java/capstone/restaurant/service/VoteService.java
+++ src/main/java/capstone/restaurant/service/VoteService.java
@@ -2,24 +2,46 @@ package capstone.restaurant.service;
 
 import capstone.restaurant.dto.vote.CreateVoteRequest;
 import capstone.restaurant.dto.vote.CreateVoteResponse;
+import capstone.restaurant.entity.Restaurant;
 import capstone.restaurant.entity.Vote;
+import capstone.restaurant.entity.VoteOption;
+import capstone.restaurant.repository.RestaurantRepository;
 import capstone.restaurant.repository.VoteOptionRepository;
 import capstone.restaurant.repository.VoteRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class VoteService {
     private final VoteRepository voteRepository;
+    private final VoteOptionRepository voteOptionRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @Transactional
     public CreateVoteResponse createVote(CreateVoteRequest createVoteRequest) {
         Vote vote = createVoteRequest.toVoteEntity();
         Vote createdVote = voteRepository.save(vote);
+
+        registerVoteOption(createVoteRequest.getRestaurants(), createdVote);
         return new CreateVoteResponse(createdVote.getVoteHash());
+    }
+
+    private void registerVoteOption(List<String> restaurants, Vote vote) {
+        restaurants.forEach(restaurantHash -> {
+            Restaurant restaurant = restaurantRepository.findByRestaurantHash(restaurantHash);
+            if (restaurant == null) {
+                throw new IllegalArgumentException(restaurantHash + " Not Found");
+            }
+            VoteOption voteOption = VoteOption.builder()
+                    .vote(vote)
+                    .restaurant(restaurant)
+                    .build();
+            voteOptionRepository.save(voteOption);
+        });
     }
 
 }
