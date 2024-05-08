cc3be66c34b50d3ff9bca2e70618ada09c5bdc5a
이광훈 <leegh963@naver.com>
2024-05-08 23:04:58 +0900

    Feat : requestDto 작성

---
+++ src/main/java/capstone/restaurant/dto/vote/createVoteUserRequest.java
@@ -0,0 +1,21 @@
+package capstone.restaurant.dto.vote;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class createVoteUserRequest {
+
+    @NotNull
+    @Schema(example = "이광훈")
+    private String userName;
+
+    @NotNull
+    @Schema(example = "1" , description = "유저가 사용하는 이미지 번호")
+    private Integer userImage;
+}
