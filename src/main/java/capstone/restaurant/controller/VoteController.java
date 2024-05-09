fc81007c1a864cf82fb5d469e8ed62a9a365de04
이광훈 <leegh963@naver.com>
2024-05-09 10:56:20 +0900

    Fix : Controller 설정

--- src/main/java/capstone/restaurant/controller/VoteController.java
+++ src/main/java/capstone/restaurant/controller/VoteController.java
@@ -4,13 +4,18 @@ import capstone.restaurant.dto.ResponseDto;
 import capstone.restaurant.dto.vote.CreateVoteRequest;
 import capstone.restaurant.dto.vote.CreateVoteResponse;
 import capstone.restaurant.dto.vote.CreateVoteUserRequest;
+import capstone.restaurant.dto.vote.CreateVoteUserResponse;
 import capstone.restaurant.service.VoteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Tag(name = "vote" , description = "투표 관련 api")
 @RestController
@@ -29,8 +34,16 @@ public class VoteController {
     @Operation(summary = "투표 참여" , description = "투표에 참여한다.")
     @PostMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseDto<> createVoteUser(@RequestBody @Validated CreateVoteUserRequest createVoteUserRequest , @PathVariable("id") String voteId){
-        voteService.createVoteUser(createVoteUserRequest , voteId);
-        return new ResponseDto<>(201 , "Created");
+    public ResponseDto<CreateVoteUserResponse> createVoteUser(@RequestBody @Validated CreateVoteUserRequest createVoteUserRequest , @PathVariable("id") String voteHash , HttpServletResponse response){
+
+        CreateVoteUserResponse createVoteUserResponse = voteService.createVoteUser(createVoteUserRequest , voteHash);
+
+        Cookie cookie = new Cookie(voteHash , createVoteUserResponse.getUserId().toString());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/api/vote/" + voteHash);
+        cookie.setMaxAge(createVoteUserResponse.getCookieDuration().intValue());
+
+        response.addCookie(cookie);
+        return new ResponseDto<>(201 , "Created" ,createVoteUserResponse);
     }
 }
