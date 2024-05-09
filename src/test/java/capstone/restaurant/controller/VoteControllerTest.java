02b0bb93da2a7bb1095509a26627ac4c6e6601d0
이광훈 <leegh963@naver.com>
2024-05-09 13:40:41 +0900

    Test : vote 테스트

--- src/test/java/capstone/restaurant/controller/VoteControllerTest.java
+++ src/test/java/capstone/restaurant/controller/VoteControllerTest.java
@@ -1,7 +1,9 @@
 package capstone.restaurant.controller;
 
 import capstone.restaurant.entity.Restaurant;
+import capstone.restaurant.entity.Vote;
 import capstone.restaurant.repository.RestaurantRepository;
+import capstone.restaurant.repository.VoteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,10 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +34,8 @@ class VoteControllerTest {
     private ObjectMapper objectMapper;
     @Autowired
     private RestaurantRepository restaurantRepository;
+    @Autowired
+    private VoteRepository voteRepository;
 
     @Test
     @DisplayName("POST /api/votes : 투표 생성 성공")
@@ -72,5 +79,43 @@ class VoteControllerTest {
                     .build();
             restaurantRepository.save(restaurant);
         }
+    }
+
+    @Test
+    @DisplayName("POST /api/votes/{id} : 투표 참여 cookie 테스트")
+    public void registerVoteUserTest() throws Exception {
+
+        registerVote();
+
+        ResultActions resultActions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/vote/123")
+                        .contentType("application/json")
+                        .content("{\"userName\": \"이광훈\", \"userImage\": 1}")
+                        .accept(MediaType.ALL)
+        );
+
+        resultActions.andExpect(status().isCreated());
+        resultActions.andExpect(cookie().exists("123"));
+    }
+
+    public void registerVote(){
+
+        Vote vote = Vote.builder().title("식당 정하기").voteHash("123").expireAt(LocalDateTime.now().plusHours(2L)).build();
+        voteRepository.save(vote);
+    }
+
+    @Test
+    @DisplayName("POST /api/votes/{id} : 없는 투표 참여 테스트")
+    public void registerVoteUserFailTest() throws Exception {
+        ResultActions resultActions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/api/vote/123")
+                        .contentType("application/json")
+                        .content("{\"userName\": \"이광훈\", \"userImage\": 1}")
+                        .accept(MediaType.ALL)
+        );
+
+        resultActions.andExpect(status().is4xxClientError());
     }
 }
