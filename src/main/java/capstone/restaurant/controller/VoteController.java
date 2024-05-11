52b2cc33336f21372008dd7532b9c993b58cd543
koomin1227 <koomin1227@naver.com>
2024-05-12 00:09:57 +0900

    Feat: deleteVoteResult 컨트롤러 구현

--- src/main/java/capstone/restaurant/controller/VoteController.java
+++ src/main/java/capstone/restaurant/controller/VoteController.java
@@ -57,7 +57,7 @@ public class VoteController {
         return new ResponseDto<>(201, "Created", null);
     }
 
-    @Operation(summary = "투표 취소", description = "사용자의 투표 내용을 전부 지운다.")
+    @Operation(summary = "투표 취소", description = "사용자의 투표 내용을 전부 지운다. 사용자가 투표 한적이 없더라도 이 요청을 보내면 오류가 발생하지 않는다. 항상 사용자 투표를 하든 안했던 안한 상태로 만든다.")
     @DeleteMapping("/{voteHash}/selection")
     public ResponseDto<Object> deleteVoteResult(
             @PathVariable("voteHash") String voteHash,
