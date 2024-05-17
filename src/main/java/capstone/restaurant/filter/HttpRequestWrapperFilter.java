6c62a7601e01aeb46ff641c51ad6ec478263add1
이광훈 <leegh963@naver.com>
2024-05-17 16:18:11 +0900

    Feat : HttpRequestWrapperFilter 를 통해 ContentWrapper 로 변환

--- src/main/java/capstone/restaurant/filter/HttpRequestWrapperFilter.java
+++ src/main/java/capstone/restaurant/filter/HttpRequestWrapperFilter.java
@@ -1,13 +1,22 @@
 package capstone.restaurant.filter;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
-public class HttpRequestWrapperFilter {
+public class HttpRequestWrapperFilter implements Filter {
 
-
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ContentCachingRequestWrapper httpRequest = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(httpRequest , servletResponse);
+    }
 }
 
 
