6e73121c17c631a9e054075f0dac1f2f1b4b2054
koomin1227 <koomin1227@naver.com>
2024-05-26 21:44:58 +0900

    Refactor: User -> Member 이름 변경

--- src/main/java/capstone/restaurant/entity/Member.java
+++ src/main/java/capstone/restaurant/entity/Member.java
@@ -1,6 +1,8 @@
 package capstone.restaurant.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @SequenceGenerator(name = "USER_SEQ_GEN" , sequenceName = "USER_SEQUENCE")
-public class User {
+public class Member {
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "USER_SEQUENCE")
     private Long id;
@@ -22,4 +24,7 @@ public class User {
     private String providerId;
 
     private int profileImage;
+    @NotNull
+    @Schema(example = "동창회 식당 정하기")
+    private String token;
 }
